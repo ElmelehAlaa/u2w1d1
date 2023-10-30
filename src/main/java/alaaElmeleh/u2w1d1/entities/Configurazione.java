126efc6faad894857ea27f644dcdf686a72e166f
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-10-30 15:09:47 +0100

    creati beans

--- src/main/java/alaaElmeleh/u2w1d1/entities/Configurazione.java
+++ src/main/java/alaaElmeleh/u2w1d1/entities/Configurazione.java
@@ -3,6 +3,9 @@ package alaaElmeleh.u2w1d1.entities;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class Configurazione {
     @Bean
@@ -18,5 +21,23 @@ public class Configurazione {
         return new Pizza("Margherita",4.99,1100);
     }
 
+    @Bean
+    public List<Toppings> hawaiiToppings() {
+        Toppings prosciutto = new Toppings("Prosciutto", 100, 2.99);
+        Toppings ananas = new Toppings("Ananas", 50, 1.49);
+
+        return Arrays.asList(prosciutto, ananas);
+    }@Bean
+    public List<Toppings> boscaiolaToppings() {
+        Toppings salsiccia = new Toppings("salsiccia", 400, 3.99);
+        Toppings funghi = new Toppings("funghi", 100, 1.79);
+
+        return Arrays.asList(salsiccia, funghi);
+    }
+
+    @Bean
+    public Pizza boscaiola(){return new Pizza("Boscaiola",5.50,1500,boscaiolaToppings());}
+    @Bean
+    Pizza Hawaii(){return new Pizza("Hawaii",5.99,1300,hawaiiToppings());}
 
 }
--- src/main/java/alaaElmeleh/u2w1d1/entities/Pizza.java
+++ src/main/java/alaaElmeleh/u2w1d1/entities/Pizza.java
@@ -12,7 +12,9 @@ public class Pizza {
     private String nome;
     private Double price;
     private int calories;
+    private List<Toppings> toppings;
 
 
-
+    public Pizza(String margherita, double price, int calories) {
+    }
 }
