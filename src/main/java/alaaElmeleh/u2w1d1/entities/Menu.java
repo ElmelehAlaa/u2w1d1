c1169ce711e09bb2ff0f768a47cfeecdffa0cf0b
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-10-30 15:39:35 +0100

    creazione classe menu

---
+++ src/main/java/alaaElmeleh/u2w1d1/entities/Menu.java
@@ -0,0 +1,11 @@
+package alaaElmeleh.u2w1d1.entities;
+
+import java.util.List;
+
+public class Menu {
+    private List<Pizza> pizze;
+    private List<Toppings> toppings;
+    private List<Bevande> bevande;
+
+}
+
